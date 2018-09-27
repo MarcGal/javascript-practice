f23254483d75020c1e71c80331b98379d1c8963b
Mar Gal <mgalloperalta@gmail.com>
2018-09-27 18:39:05 +0200

    praxcticing operators

---
+++ main.js
@@ -0,0 +1,25 @@
+// var name = 'marco';
+
+// console.log(name);
+
+// var sum = 4 + 5;
+
+// console.log(sum);
+
+// console.log('my name is VERGON');
+
+// console.log(6 / 2);
+
+// console.log(8 % 2 === 0);
+
+var expressionOne = ((2 * 2) + 5) * 6;
+// console.log(expressionOne);
+
+var expressionTwo = ((2* 2) + (5 * 3)) - 5;
+// console.log(expressionTwo);
+
+var expressionThree = (5 * 5) / (5 * 5);
+// console.log(expressionThree);
+
+var expressionFour = 5 * 5 - 5 * 4;
+// console.log(expressionFour);
