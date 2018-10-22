811dda87030cfbdddbe1bf6d698f7e874477b0fe
Mar Gal <mgalloperalta@gmail.com>
2018-10-22 22:43:58 +0200

    practicing math random and math floor, returning function values

--- functions.js
+++ functions.js
@@ -1,18 +1,45 @@
-function sayHelloManyTimes(name, howManyTimes) {
-  for (var i=0; i < howManyTimes; i++) {
-    console.log('Hello ' + name + '!');
-  }
+// function sayHelloManyTimes(name, howManyTimes) {
+//   for (var i=0; i < howManyTimes; i++) {
+//     console.log('Hello ' + name + '!');
+//   }
+// }
+
+// sayHelloManyTimes('Marco', 4);
+// sayHelloManyTimes(3,'error');
+
+// sayHelloManyTimes(3, 'ERROR');
+// // Will this work? NO
+
+// sayHelloManyTimes(2);
+// // Will this work? NO
+
+// sayHelloManyTimes('ironBrain');
+// // Will this work? NO
+
+// ------------------ PRACTICING RETURNING VALUES ---------------------
+
+/*
+function anotherFunction(text){
+  console.log("Hello "+ text);
 }
 
-sayHelloManyTimes('Marco', 4);
-sayHelloManyTimes(3,'error');
+function oneFunction(name) {
+  return anotherFunction(name);
+}
 
-sayHelloManyTimes(3, 'ERROR');
-// Will this work? NO
+oneFunction("Lluis");
+*/
 
-sayHelloManyTimes(2);
-// Will this work? NO
+function gradeTheStudent () {
+  var grades = ['Master', 'Good', 'Acceptable', 'Average', 'Poor', 'Fail'];
 
-sayHelloManyTimes('ironBrain');
-// Will this work? NO
+  var randomNumber = Math.random();            // Random decimal [0.0 - 1.0)
+  randomNumber = randomNumber * grades.length; // Random decimal [0.0 - 6.0)
+  randomNumber = Math.floor(randomNumber);     // Random integer [0   - 5]
+
+  return grades[randomNumber];
+}
+
+var myGrade = gradeTheStudent();
+console.log(myGrade);
 
