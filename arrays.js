2bd22b02c42410244354b490790276a6836ba656
Mar Gal <mgalloperalta@gmail.com>
2018-10-23 21:30:58 +0200

    practicing push and splice methods on array

--- arrays.js
+++ arrays.js
@@ -1,4 +1,20 @@
+//------------------- Push method on array ----------------
 
+var arrayNames = ["Pedro", "Jake", "Joan"];
+
+arrayNames.push("Rachel");
+
+console.log(arrayNames[3]);
+
+
+
+//------------------- Splice method on Array ----------------
+
+var arrayNames = ["Pedro", "Jake", "Joan"];
+
+console.log(arrayNames[0]);
+arrayNames.splice(0,1);
+console.log(arrayNames[0]);
 
 //------------------- For Each Loop pn Array ----------------
 function printStars(howMany){
@@ -8,4 +24,14 @@ function printStars(howMany){
 [1,2,3,4,5].forEach(function(num){
   printStars(num)
 });
+
+//------------------- Split function on string----------------
+
+var phrase = "This is long enough for a string to count it words";
+
+var words = phrase.split(" ");
+
+console.log(words);
+console.log(words[0]);
+console.log(words.length);
 
