5e97749f85fa176fbd2fd73e21e47299b5d9cd01
Mar Gal <mgalloperalta@gmail.com>
2018-10-25 18:09:27 +0200

    practicing array methods

--- arrays.js
+++ arrays.js
@@ -1,37 +1,47 @@
-//------------------- Push method on array ----------------
+// //------------------- Push method on array ----------------
 
-var arrayNames = ["Pedro", "Jake", "Joan"];
+// var arrayNames = ["Pedro", "Jake", "Joan"];
 
-arrayNames.push("Rachel");
+// arrayNames.push("Rachel");
 
-console.log(arrayNames[3]);
+// console.log(arrayNames[3]);
 
 
 
-//------------------- Splice method on Array ----------------
+// //------------------- Splice method on Array ----------------
 
-var arrayNames = ["Pedro", "Jake", "Joan"];
+// var arrayNames = ["Pedro", "Jake", "Joan"];
 
-console.log(arrayNames[0]);
-arrayNames.splice(0,1);
-console.log(arrayNames[0]);
+// console.log(arrayNames[0]);
+// arrayNames.splice(0,1);
+// console.log(arrayNames[0]);
 
-//------------------- For Each Loop pn Array ----------------
-function printStars(howMany){
-  console.log("*".repeat(howMany));
+// //------------------- For Each Loop pn Array ----------------
+// function printStars(howMany){
+//   console.log("*".repeat(howMany));
+// }
+
+// [1,2,3,4,5].forEach(function(num){
+//   printStars(num)
+// });
+
+// //------------------- Split function on stringls----------------
+
+// var phrase = "This is long enough for a string to count it words";
+
+// var words = phrase.split(" ");
+
+// console.log(words);
+// console.log(words[0]);
+// console.log(words.length);
+
+var array = ['marco', 'felix', 'audrius', 'xevi'];
+var arraysLengths = 0;
+
+for (var i = 0; i < array.length; i++){
+  arraysLengths += array[i].length; 
+  console.log(arraysLengths/array.length);
 }
 
-[1,2,3,4,5].forEach(function(num){
-  printStars(num)
-});
 
-//------------------- Split function on stringls----------------
-
-var phrase = "This is long enough for a string to count it words";
-
-var words = phrase.split(" ");
-
-console.log(words);
-console.log(words[0]);
-console.log(words.length);
 
