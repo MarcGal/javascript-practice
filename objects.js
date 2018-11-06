7f8c446c6f5f8ed25badff423855e1ebf251e04b
Mar Gal <mgalloperalta@gmail.com>
2018-11-06 16:12:36 +0100

    practicing adding and removing object properties

--- objects.js
+++ objects.js
@@ -1,6 +1,31 @@
+//declaring an object, objects are literals and can be stored in variable
+
+
 var olympicRecords = {
   athletics100Men: "Justin Gatlin",
   athleticsLongJumpMen: "Mike Powel"
-}
+};
 
-console.log(olympicRecords.athleticsLongJumpMen);
+//console.log(olympicRecords.athleticsLongJumpMen);
+
+
+// Adding keys and values with dot dotation
+
+olympicRecords.swimming200Men = "Michael Phelps";
+
+
+// Adding keys and values with bracket dotation
+
+olympicRecords.swimming400Women= "Katie Ledecky";
+
+olympicRecords.doubleOllie = "Chris Chann";
+
+
+
+
+// ---------- Deleting key-value pairs
+
+delete olympicRecords.doubleOllie;
+
+console.log(olympicRecords);
+
